e42c39bbc9d8cb254d137a55c50b68c6317a28bd
senchkim <himidoricode@gmail.com>
2020-02-11 15:27:49 +0300

    Выполняет задание module3-task1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
+import questions from "./mocks/questions.js";
 
 const errorsAmount = 3;
 
 ReactDOM.render(
-    <App errorsAmount={errorsAmount}/>,
+    <App
+      errorsAmount={errorsAmount}
+      questions={questions}
+    />,
     document.getElementById(`root`)
 );
